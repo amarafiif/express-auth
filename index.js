7f1e2f2fde4ed6fb6ac8c2c6da88679a6bab0ecd
amarafiif <amarafif230@gmail.com>
2023-11-17 23:41:13 +0700

    feat: integrate socket.io

--- index.js
+++ index.js
@@ -2,10 +2,12 @@ const express = require("express"),
 	cors = require("cors"),
 	app = express(),
 	port = process.env.PORT || 3000,
-	router = require("./routers");
-bodyParser = require("body-parser");
-const { configSentry } = require("./utils");
-const Sentry = require("@sentry/node");
+	router = require("./routers"),
+	bodyParser = require("body-parser"),
+	{ configSentry } = require("./utils"),
+	Sentry = require("@sentry/node"),
+	http = require("http").Server(app),
+	io = require("socket.io")(http);
 
 require("dotenv").config();
 configSentry();
@@ -17,7 +19,16 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(Sentry.Handlers.requestHandler());
 app.use(Sentry.Handlers.tracingHandler());
 app.use(router);
+
+io.on("connection", (socket) => {
+	console.log("A user connected");
+
+	socket.on("disconnect", () => {
+		console.log("User disconnected");
+	});
+});
+
 app.use(Sentry.Handlers.errorHandler());
-app.listen(port, () => {
-	console.log(`Server running at https://localhost:${port}`);
+http.listen(port, () => {
+	console.log(`Server running at http://localhost:${port}`);
 });
